885413b1de0f013b6b19f15e8d53e3e1c92c9dc7
SymbolixAU <dcooley@symbolix.com.au>
2018-12-07 15:55:27 +1100

    tests

--- tests/testthat/test-geojson.R
+++ tests/testthat/test-geojson.R
@@ -32,75 +32,69 @@ test_that("geometry field changes with user input", {
 
 test_that("MULTI objects are down-casted", {
 
-  # pt1 <- sf::st_sf( geometry = sf::st_sfc( sf::st_point(c(0,0))))
-  # mp1 <- sf::st_sf( geometry = sf::st_sfc( sf::st_multipoint(x = matrix(1:6, ncol = 2))))
-  # mp2 <- sf::st_sf( geometry = sf::st_sfc( sf::st_multipoint(x = matrix(1:6, ncol = 3))))
-  # mp3 <- sf::st_sf( geometry = sf::st_sfc( sf::st_multipoint(x = matrix(1:8, ncol = 4))))
-  # pt2 <- sf::st_sf( geometry = sf::st_sfc( sf::st_point(c(1,1,3,4))))
-  # ls1 <- sf::st_sf( geometry = sf::st_sfc( sf::st_linestring(x = matrix(1:6, ncol = 2))))
-  # ls2 <- sf::st_sf( geometry = sf::st_sfc( sf::st_linestring(x = matrix(1:6, ncol = 3))))
-  # mls1 <- sf::st_sf( geometry = sf::st_sfc( sf::st_multilinestring(x = list(matrix(1:6, ncol = 2), matrix(6:1, ncol = 2), matrix(1:6, ncol = 2)))))
-  #
-  # js <- '{"type":"MultiPolygon","coordinates":[[[[0.0,0.0],[0.0,1.0],[1.0,1.0],[1.0,0.0],[0.0,0.0]],[[0.5,0.5],[0.5,0.75],[0.75,0.75],[0.75,0.5],[0.5,0.5]]],[[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]]}'
-  # mp <- geojsonsf::geojson_sf( js )
-  # sf::st_crs( mp ) <- sf::st_crs( mls1 )
-  #
-  # geojsonsf::sf_geojson(pt1)
-  # sf <- rbind(pt1, mp1, mp2, mp3, pt2, ls1, ls2, mls1, mp)
-  # sf$id <- 1:nrow(sf)
-  #
-  # geojsonsf::sf_geojson( sf[8, ] , atomise = T)
-
-
   ## TODO( redo these tests, because spatialwidget down't return valid GeoJSON, so the geojsonsf::geojson_sf() errors)
-  # js <- '{"type":"Feature","properties":{"id":2},"geometry":{"type":"MultiPoint","coordinates":[[1,4],[2,5],[3,6]]}}'
-  # sf <- geojsonsf::geojson_sf( js )
-  # geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
-  #
-  # expect_true(jsonify:::validate_json(geo_down))
-  # sf_down <- geojsonsf::geojson_sf( geo_down )
-  # expect_true(nrow(sf_down) == 3)
-  #
-  # js <- '{"type":"Feature","properties":{"id":3},"geometry":{"type":"MultiPoint","coordinates":[[1,3,5],[2,4,6]]}}'
-  # sf <- geojsonsf::geojson_sf( js )
-  # geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
-  #
-  # expect_true(jsonify:::validate_json(geo_down))
-  # sf_down <- geojsonsf::geojson_sf( geo_down )
-  # expect_true(nrow(sf_down) == 2)
-  #
-  #
-  # js <- '{"type":"Feature","properties":{"id":4},"geometry":{"type":"MultiPoint","coordinates":[[1,3,5,7],[2,4,6,8]]}}'
-  # sf <- geojsonsf::geojson_sf( js )
-  # geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
-  #
-  # expect_true(jsonify:::validate_json(geo_down))
-  # sf_down <- geojsonsf::geojson_sf( geo_down )
-  # expect_true(nrow(sf_down) == 2)
-  #
-  # js <- '{"type":"Feature","properties":{"id":8},"geometry":{"type":"MultiLineString","coordinates":[[[1.0,4.0],[2.0,5.0],[3.0,6.0]],[[6.0,3.0],[5.0,2.0],[4.0,1.0]],[[1.0,4.0],[2.0,5.0],[3.0,6.0]]]}}'
-  # sf <- geojsonsf::geojson_sf( js )
-  # geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
-  #
-  # expect_true(jsonify:::validate_json(geo_down))
-  # sf_down <- geojsonsf::geojson_sf( geo_down )
-  # expect_true(nrow(sf_down) == 3)
-  #
-  #
-  # js <- '{"type":"MultiPolygon","coordinates":[[[[0.0,0.0],[0.0,1.0],[1.0,1.0],[1.0,0.0],[0.0,0.0]],[[0.5,0.5],[0.5,0.75],[0.75,0.75],[0.75,0.5],[0.5,0.5]]],[[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]]}'
-  # sf <- geojsonsf::geojson_sf( js )
-  #
-  # geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
-  # expect_true(jsonify:::validate_json(geo_down))
-  # sf_down <- geojsonsf::geojson_sf( geo_down )
-  # expect_true(nrow(sf_down) == 2)
-  #
-  # ## a combination of some of the above
-  # js <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{"id":2},"geometry":{"type":"MultiPoint","coordinates":[[1,4],[2,5],[3,6]]}},{"type":"Feature","properties":{"id":8},"geometry":{"type":"MultiLineString","coordinates":[[[1.0,4.0],[2.0,5.0],[3.0,6.0]],[[6.0,3.0],[5.0,2.0],[4.0,1.0]],[[1.0,4.0],[2.0,5.0],[3.0,6.0]]]}},{"type":"Feature","properties":{"id":9},"geometry":{"type":"MultiPolygon","coordinates":[[[[0.0,0.0],[0.0,1.0],[1.0,1.0],[1.0,0.0],[0.0,0.0]],[[0.5,0.5],[0.5,0.75],[0.75,0.75],[0.75,0.5],[0.5,0.5]]],[[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]]}}]}'
-  # sf <- geojsonsf::geojson_sf( js )
-  # geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
-  # sf <- geojsonsf::geojson_sf( geo_down )
-  #
-  # expect_true( nrow(sf) == 8 )
-  # expect_true( length(unique(sf$id)) == 3 )
+  js <- '{"type":"Feature","properties":{},"geometry":{"type":"MultiPoint","coordinates":[[1,4],[2,5]]}}'
+  sf <- geojsonsf::geojson_sf( js )
+  geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
+  expected <- '[{"type":"Feature","properties":{},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,4.0]}}},
+  {"type":"Feature","properties":{},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,5.0]}}}]'
+  expect_true(jsonify:::validate_json(geo_down))
+  expect_equal( as.character( geo_down ), gsub("\\n| ","", expected ) )
+
+  js <- '{"type":"MultiPoint","coordinates":[[1,3,5,7],[2,4,6,8]]}'
+  sf <- geojsonsf::geojson_sf( js )
+  geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
+  expected <- '[{"type":"Feature","properties":{},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,3.0,5.0,7.0]}}},
+  {"type":"Feature","properties":{},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,4.0,6.0,8.0]}}}]'
+  expect_true(jsonify:::validate_json(geo_down))
+  expect_equal( as.character( geo_down ), gsub("\\n| ","", expected ) )
+
+  js <- '{"type":"Feature","properties":{"id":8},"geometry":{"type":"MultiLineString","coordinates":[[[1.0,4.0],[2.0,5.0]],[[6.0,3.0],[5.0,2.0]]]}}'
+  sf <- geojsonsf::geojson_sf( js )
+  geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
+  expected <- '[{"type":"Feature","properties":{"id":8.0},"geometry":{"geometry":  {"type":"LineString","coordinates":[[1.0,4.0],[2.0,5.0]]}}},{"type":"Feature","properties":{"id":8.0},"geometry":{"geometry":{"type":"LineString","coordinates":[[6.0,3.0],[5.0,2.0]]}}}]'
+  expect_true(jsonify:::validate_json(geo_down))
+  expect_equal( as.character( geo_down ), gsub("\\n| ","", expected ) )
+
+
+  js <- '{"type":"MultiPolygon","coordinates":[[[[0.0,0.0],[0.0,1.0],[1.0,1.0],[1.0,0.0],[0.0,0.0]],[[0.5,0.5],[0.5,0.75],[0.75,0.75],[0.75,0.5],[0.5,0.5]]],[[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]]}'
+  sf <- geojsonsf::geojson_sf( js )
+  geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
+  expected <- '[{"type":"Feature","properties":{},"geometry":{"geometry":{"type":"Polygon","coordinates":[[[0.0,0.0],[0.0,1.0],[1.0,1.0],[1.0,0.0],[0.0,0.0]],[[0.5,0.5],[0.5,0.75],[0.75,0.75],[0.75,0.5],[0.5,0.5]]]}}},{"type":"Feature","properties":{},"geometry":{"geometry":{"type":"Polygon","coordinates":[[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]}}}]'
+  expect_true(jsonify:::validate_json(geo_down))
+  expect_equal( as.character( geo_down ), gsub("\\n| ","", expected ) )
+
+  ## a combination of some of the above
+  js <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{"id":2},"geometry":{"type":"MultiPoint","coordinates":[[1,4],[2,5],[3,6]]}},{"type":"Feature","properties":{"id":8},"geometry":{"type":"MultiLineString","coordinates":[[[1.0,4.0],[2.0,5.0],[3.0,6.0]],[[6.0,3.0],[5.0,2.0],[4.0,1.0]],[[1.0,4.0],[2.0,5.0],[3.0,6.0]]]}},{"type":"Feature","properties":{"id":9},"geometry":{"type":"MultiPolygon","coordinates":[[[[0.0,0.0],[0.0,1.0],[1.0,1.0],[1.0,0.0],[0.0,0.0]],[[0.5,0.5],[0.5,0.75],[0.75,0.75],[0.75,0.5],[0.5,0.5]]],[[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]]}}]}'
+  sf <- geojsonsf::geojson_sf( js )
+  geo_down <- spatialwidget:::rcpp_sf_to_geojson_downcast( sf, "geometry" )
+  expected <- '[{"type":"Feature","properties":{"id":2.0},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,4.0]}}},{"type":"Feature","properties":{"id":2.0},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,5.0]}}},{"type":"Feature","properties":{"id":2.0},"geometry":{"geometry":{"type":"Point","coordinates":[3.0,6.0]}}},{"type":"Feature","properties":{"id":8.0},"geometry":{"geometry":{"type":"LineString","coordinates":[[1.0,4.0],[2.0,5.0],[3.0,6.0]]}}},{"type":"Feature","properties":{"id":8.0},"geometry":{"geometry":{"type":"LineString","coordinates":[[6.0,3.0],[5.0,2.0],[4.0,1.0]]}}},{"type":"Feature","properties":{"id":8.0},"geometry":{"geometry":{"type":"LineString","coordinates":[[1.0,4.0],[2.0,5.0],[3.0,6.0]]}}},{"type":"Feature","properties":{"id":9.0},"geometry":{"geometry":{"type":"Polygon","coordinates":[[[0.0,0.0],[0.0,1.0],[1.0,1.0],[1.0,0.0],[0.0,0.0]],[[0.5,0.5],[0.5,0.75],[0.75,0.75],[0.75,0.5],[0.5,0.5]]]}}},{"type":"Feature","properties":{"id":9.0},"geometry":{"geometry":{"type":"Polygon","coordinates":[[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]}}}]'
+  expect_true(jsonify:::validate_json(geo_down))
+  expect_equal( as.character( geo_down ), gsub("\\n| ","", expected ) )
 })
+
+test_that("MULTI OD sf objects are downcast and atomised", {
+
+
+  geo1 <- '[
+{"type":"MultiPoint","coordinates":[[0,0],[1,1],[2,2]]},
+{"type":"MultiPoint","coordinates":[[0,0],[1,1],[2,2],[3,3]]}
+]'
+  geo2 <- '[
+{"type":"MultiPoint","coordinates":[[9,9],[8,8],[7,7],[6,6],[5,5]]},
+{"type":"MultiPoint","coordinates":[[9,9]]}
+]'
+
+
+  sf1 <- geojsonsf::geojson_sf( geo1 )
+  sf2 <- geojsonsf::geojson_sf( geo2 )
+
+  sf <- cbind( sf1, sf2 )
+  sf$id <- 1:nrow(sf)
+  geo_down <- spatialwidget:::rcpp_sf_to_geojson_multi_column_downcast( sf, c("geometry", "geometry.1"))
+  expected <- '[{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[0.0,0.0]},"geometry":{"type":"Point","coordinates":[9.0,9.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,1.0]},"geometry":{"type":"Point","coordinates":[9.0,9.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,2.0]},"geometry":{"type":"Point","coordinates":[9.0,9.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[0.0,0.0]},"geometry":{"type":"Point","coordinates":[8.0,8.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,1.0]},"geometry":{"type":"Point","coordinates":[8.0,8.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,2.0]},"geometry":{"type":"Point","coordinates":[8.0,8.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[0.0,0.0]},"geometry":{"type":"Point","coordinates":[7.0,7.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,1.0]},"geometry":{"type":"Point","coordinates":[7.0,7.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,2.0]},"geometry":{"type":"Point","coordinates":[7.0,7.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[0.0,0.0]},"geometry":{"type":"Point","coordinates":[6.0,6.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,1.0]},"geometry":{"type":"Point","coordinates":[6.0,6.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,2.0]},"geometry":{"type":"Point","coordinates":[6.0,6.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[0.0,0.0]},"geometry":{"type":"Point","coordinates":[5.0,5.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,1.0]},"geometry":{"type":"Point","coordinates":[5.0,5.0]}}},{"type":"Feature","properties":{"id":1},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,2.0]},"geometry":{"type":"Point","coordinates":[5.0,5.0]}}},{"type":"Feature","properties":{"id":2},"geometry":{"geometry":{"type":"Point","coordinates":[0.0,0.0]},"geometry":{"type":"Point","coordinates":[9.0,9.0]}}},{"type":"Feature","properties":{"id":2},"geometry":{"geometry":{"type":"Point","coordinates":[1.0,1.0]},"geometry":{"type":"Point","coordinates":[9.0,9.0]}}},{"type":"Feature","properties":{"id":2},"geometry":{"geometry":{"type":"Point","coordinates":[2.0,2.0]},"geometry":{"type":"Point","coordinates":[9.0,9.0]}}},{"type":"Feature","properties":{"id":2},"geometry":{"geometry":{"type":"Point","coordinates":[3.0,3.0]},"geometry":{"type":"Point","coordinates":[9.0,9.0]}}}]'
+  expect_true(jsonify:::validate_json(geo_down))
+  expect_equal( as.character( geo_down ), gsub("\\n| ","", expected ) )
+
+})
+
